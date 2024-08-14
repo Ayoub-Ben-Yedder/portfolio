5d50111522249c1543410512c630cfac0664edf3
Ayoub  Ben_Yedder <benmimounlamia@gmail.com>
2024-08-14 21:05:48 +0100

    disabled buggy cursor animation on touchscreens

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,103 +1,92 @@
-const cursorCanvas = document.getElementById("cursor");
-const context = cursorCanvas.getContext('2d');
-
-// for intro motion
-let mouseMoved = false;
-
-const pointer = {
-    x: .5 * window.innerWidth,
-    y: .5 * window.innerHeight,
-}
-const params = {
-    pointsNumber: 20,
-    widthFactor: 1,
-    mouseThreshold: .6,
-    spring: .6,
-    friction: .4
-};
-
-const trail = new Array(params.pointsNumber);
-for (let i = 0; i < params.pointsNumber; i++) {
-    trail[i] = {
-        x: pointer.x,
-        y: pointer.y,
-        dx: 0,
-        dy: 0,
-    }
-}
-var color;
-
-
-window.addEventListener("click", e => {
-    console.log(e);
-    updateMousePosition(e.clientX, e.clientY);
-});
-window.addEventListener("mousemove", e => {
-    mouseMoved = true;
-    updateMousePosition(e.clientX, e.clientY);
-    var anchor = e.target.closest('a');
-    if(anchor !== null) {
-      color = "#EB6E93";
-    }else{
-      color = "#C4A6E6";
-    }
-});
-window.addEventListener("touchmove", e => {
-    mouseMoved = true;
-    updateMousePosition(e.targetTouches[0].clientX, e.targetTouches[0].clientY);
-});
-
-function updateMousePosition(eX, eY) {
-    pointer.x = eX;
-    pointer.y = eY;
-}
-
-setupCanvas();
-update(0);
-window.addEventListener("resize", setupCanvas);
-
-
-function update(t) {
+if (!window.matchMedia("(pointer: coarse)").matches) {
+    // touchscreen
+    const cursorCanvas = document.getElementById("cursor");
+    const context = cursorCanvas.getContext('2d');
 
     // for intro motion
-    if (!mouseMoved) {
-        pointer.x = (.5 + .3 * Math.cos(.002 * t) * (Math.sin(.005 * t))) * window.innerWidth;
-        pointer.y = (.5 + .2 * (Math.cos(.005 * t)) + .1 * Math.cos(.01 * t)) * window.innerHeight;
-    }
+    let mouseMoved = false;
 
-    context.clearRect(0, 0, cursorCanvas.width, cursorCanvas.height);
-    trail.forEach((p, pIdx) => {
-        const prev = pIdx === 0 ? pointer : trail[pIdx - 1];
-        const spring = pIdx === 0 ? .4 * params.spring : params.spring;
-        p.dx += (prev.x - p.x) * spring;
-        p.dy += (prev.y - p.y) * spring;
-        p.dx *= params.friction;
-        p.dy *= params.friction;
-        p.x += p.dx;
-        p.y += p.dy;
+    const pointer = {
+        x: .5 * window.innerWidth,
+        y: .5 * window.innerHeight,
+    }
+    const params = {
+        pointsNumber: 20,
+        widthFactor: 1,
+        mouseThreshold: .6,
+        spring: .6,
+        friction: .4
+    };
+
+    const trail = new Array(params.pointsNumber);
+    for (let i = 0; i < params.pointsNumber; i++) {
+        trail[i] = {
+            x: pointer.x,
+            y: pointer.y,
+            dx: 0,
+            dy: 0,
+        }
+    }
+    var color;
+    window.addEventListener("mousemove", e => {
+        mouseMoved = true;
+        updateMousePosition(e.clientX, e.clientY);
+        var anchor = e.target.closest('a');
+        if (anchor !== null) {
+            color = "#EB6E93";
+        } else {
+            color = "#C4A6E6";
+        }
     });
 
-    context.lineCap = "round";
-    context.strokeStyle = color;
-    context.beginPath();
-    context.moveTo(trail[0].x, trail[0].y);
-
-    for (let i = 1; i < trail.length - 1; i++) {
-        const xc = .5 * (trail[i].x + trail[i + 1].x);
-        const yc = .5 * (trail[i].y + trail[i + 1].y);
-        context.quadraticCurveTo(trail[i].x, trail[i].y, xc, yc);
-        context.lineWidth = params.widthFactor * (params.pointsNumber - i);
-        context.stroke();
+    function updateMousePosition(eX, eY) {
+        pointer.x = eX;
+        pointer.y = eY;
     }
-    context.lineTo(trail[trail.length - 1].x, trail[trail.length - 1].y);
-    context.stroke();
 
-    window.requestAnimationFrame(update);
-}
-
-function setupCanvas() {
-    cursorCanvas.width = window.innerWidth;
-    cursorCanvas.height = window.innerHeight;
+    setupCanvas();
+    update(0);
+    window.addEventListener("resize", setupCanvas);
 
 
+    function update(t) {
+        if (!mouseMoved) {
+            pointer.x = (.5 + .3 * Math.cos(.002 * t) * (Math.sin(.005 * t))) * window.innerWidth;
+            pointer.y = (.5 + .2 * (Math.cos(.005 * t)) + .1 * Math.cos(.01 * t)) * window.innerHeight;
+        }
+
+        context.clearRect(0, 0, cursorCanvas.width, cursorCanvas.height);
+        trail.forEach((p, pIdx) => {
+            const prev = pIdx === 0 ? pointer : trail[pIdx - 1];
+            const spring = pIdx === 0 ? .4 * params.spring : params.spring;
+            p.dx += (prev.x - p.x) * spring;
+            p.dy += (prev.y - p.y) * spring;
+            p.dx *= params.friction;
+            p.dy *= params.friction;
+            p.x += p.dx;
+            p.y += p.dy;
+        });
+
+        context.lineCap = "round";
+        context.strokeStyle = color;
+        context.beginPath();
+        context.moveTo(trail[0].x, trail[0].y);
+
+        for (let i = 1; i < trail.length - 1; i++) {
+            const xc = .5 * (trail[i].x + trail[i + 1].x);
+            const yc = .5 * (trail[i].y + trail[i + 1].y);
+            context.quadraticCurveTo(trail[i].x, trail[i].y, xc, yc);
+            context.lineWidth = params.widthFactor * (params.pointsNumber - i);
+            context.stroke();
+        }
+        context.lineTo(trail[trail.length - 1].x, trail[trail.length - 1].y);
+        context.stroke();
+
+        window.requestAnimationFrame(update);
+    }
+
+    function setupCanvas() {
+        cursorCanvas.width = window.innerWidth;
+        cursorCanvas.height = window.innerHeight;
+    }
 }
